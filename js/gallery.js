953a46303f98532f7f4d1279f53615530dd6998b
ShiHou <41864212+GHOST22875@users.noreply.github.com>
2025-12-03 18:52:35 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -5,10 +5,11 @@ const galleries = {
         location: "Загородный дом, Московская область",
         description: "Просторная терраса из древесно-полимерного композита площадью 45 м². Современный дизайн с минималистичным подходом.",
         images: [
-            "https://i.pinimg.com/736x/0f/92/c5/0f92c5f1ee2bce5d0ee792415b6ef6eb.jpg",
-            "https://i.pinimg.com/736x/9e/47/cb/9e47cb699e37288fa745f2868f40a0e8.jpg",
-            "https://i.pinimg.com/736x/d4/26/3b/d4263bb93d82a2bea3ba25a3573b45a8.jpg",
-            "https://i.pinimg.com/736x/a5/7b/38/a57b38b8ab04dd83e66895bb3d96d978.jpg"
+            "/image/facade1/5256075123540822991.jpg",
+            "/image/facade1/5256075123540822992.jpg",
+            "/image/facade1/5256075123540822993.jpg",
+            "/image/facade1/5256075123540822994.jpg",
+            "/image/facade1/5256075123540822995.jpg"
         ]
     },
     project2: {
@@ -16,9 +17,16 @@ const galleries = {
         location: "Дачный участок, Ленинградская область",
         description: "Уютная терраса из сибирской лиственницы площадью 32 м² с классическим дизайном.",
         images: [
-            "https://i.pinimg.com/736x/b8/68/cf/b868cf28c80d5fc663ab01344d0a5534.jpg",
-            "https://i.pinimg.com/736x/23/fb/80/23fb80e93698635dc44b2fb9c664da3c.jpg",
-            "https://i.pinimg.com/736x/0f/92/c5/0f92c5f1ee2bce5d0ee792415b6ef6eb.jpg"
+            "/image/facade2/5256075123540823006.jpg",
+            "/image/facade2/5256075123540823007.jpg",
+            "/image/facade2/5256075123540823008.jpg",
+            "/image/facade2/5256075123540823009.jpg",
+            "/image/facade2/5256075123540823010.jpg",
+            "/image/facade2/5256075123540823011.jpg",
+            "/image/facade2/5256075123540823012.jpg",
+            "/image/facade2/5256075123540823013.jpg",
+            "/image/facade2/5256075123540823014.jpg",
+            "/image/facade2/5256075123540823016.jpg"
         ]
     },
     project3: {
@@ -26,9 +34,11 @@ const galleries = {
         location: "Частный дом, Калужская область",
         description: "Терраса площадью 38 м² с комбинированным покрытием из ДПК и натурального камня.",
         images: [
-            "https://i.pinimg.com/736x/a5/7b/38/a57b38b8ab04dd83e66895bb3d96d978.jpg",
-            "https://i.pinimg.com/736x/d4/26/3b/d4263bb93d82a2bea3ba25a3573b45a8.jpg",
-            "https://i.pinimg.com/736x/b8/68/cf/b868cf28c80d5fc663ab01344d0a5534.jpg"
+            "/image/facade3/photo_2025-11-19_10-39-37.jpg",
+            "/image/facade3/photo_2025-11-19_10-39-57.jpg",
+            "/image/facade3/photo_2025-11-19_10-39-58.jpg",
+            "/image/facade3/photo_2025-11-19_10-39-59.jpg",
+            "/image/facade3/photo_2025-11-19_10-40-03.jpg"
         ]
     },
     project4: {
@@ -36,9 +46,10 @@ const galleries = {
         location: "Коттеджный поселок, Новорижское шоссе",
         description: "Терраса площадью 50 м² с современным навесом из поликарбоната.",
         images: [
-            "https://i.pinimg.com/736x/9e/47/cb/9e47cb699e37288fa745f2868f40a0e8.jpg",
-            "https://i.pinimg.com/736x/0f/92/c5/0f92c5f1ee2bce5d0ee792415b6ef6eb.jpg",
-            "https://i.pinimg.com/736x/a5/7b/38/a57b38b8ab04dd83e66895bb3d96d978.jpg"
+            "/image/facade4/photo_2025-11-19_10-40-21.jpg",
+            "/image/facade4/photo_2025-11-19_10-40-23.jpg",
+            "/image/facade4/photo_2025-11-19_10-40-24.jpg",
+            "/image/facade4/photo_2025-11-19_10-40-26.jpg"
         ]
     },
     project5: {
